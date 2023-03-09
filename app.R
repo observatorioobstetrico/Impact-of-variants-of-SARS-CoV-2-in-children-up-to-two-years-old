ead9630e409da600f572edaa3bcaeb504dfb68d0
José Carlos S.J <51521509+Soju-JC@users.noreply.github.com>
2023-03-08 22:57:07 -0300

    Analise da variável idade/remoção das abas smote e downsample

--- app.R
+++ app.R
@@ -25,7 +25,7 @@ library(tidymodels)
 library(themis)
 library(dplyr)
 library(glmtoolbox)
-
+library(sjPlot)
 # Load dataset
 data5 <- readRDS("data_paper_1000d.rds")
 
@@ -41,39 +41,6 @@ data5$CLASSI_FIN <- as.factor(data5$CLASSI_FIN)
 
 data5$DT_SIN_PRI <- dmy(data5$DT_SIN_PRI)
 data5$DT_EVOLUCA <- dmy(data5$DT_EVOLUCA)
-
-
-# Smote
-dados_smote <- function(dados,var){
-  dados1 <- dados %>% select(variants, var) %>% drop_na()
-  
-  # set.seed(69)
-  
-  ds_rec <- recipe(variants ~., data = dados1) %>%
-    step_smotenc(variants,seed=69) %>%
-    prep()
-  
-  new_data <- juice(ds_rec)
-  
-  return(new_data)
-  
-}
-
-# Downsample
-dados_downsample <- function(dados,var){
-  dados1 <- dados %>% select(variants, var) %>% drop_na()
-  
-  # set.seed(69)
-  
-  ds_rec <- recipe(variants ~., data = dados1) %>%
-    step_downsample(variants,seed = 69) %>%
-    prep()
-  
-  new_data <- juice(ds_rec)
-  
-  return(new_data)
-  
-}
 
 sticky_style <-
   list(
@@ -116,9 +83,8 @@ ui <-
         style = "position: fixed; overflow: visible;",
         menuItem("Documentation", tabName = "doc"),
         menuItem("General Analysis", tabName = "tab_cruzada"),
-        menuItem("Models", tabName = "modelos"),
-        menuItem("Analysis with Smote", tabName = "anal_var"),
-        menuItem("Analysis with Downsample", tabName = "anal_downsample")
+        menuItem("Age analysis", tabName = "agn"),
+        menuItem("Models", tabName = "modelos")
       )
     ),
     ### body ----
@@ -261,170 +227,11 @@ ui <-
                   )
                   )
                 )),
-        ### Analysis with Smote ----
-        tabItem(tabName = "anal_var",
-                fluidRow(
-                  box(
-                    collapsible = TRUE,
-                    width = 4,
-                    title = "Select",
-                    status = "primary",
-                    solidHeader = FALSE,
-                    selectInput(
-                      inputId = "caracteristicas2",
-                      label = "Row variable:",
-                      choices = c(
-                        "Brazil region"  = "region",
-                        "Federative unit of Brazil"  = "SG_UF",
-                        "Ethnicity" = "ethnicity",
-                        "Sex" = "sex",
-                        "Travel history" = "travel_hist",
-                        "Change of municipality for service" = "changed_muni",
-                        "Area of residence" = "zone",
-                        "Flu syndrome evolved into SRAG" = "flu_into_srag",
-                        "Infection acquired in the hospital" = "inf_inter",
-                        "Contact with poultry or swine" = "cont_pty_swine",
-                        "Flu vaccine" = "vacine",
-                        "Antiviral" = "antiviral",
-                        "Fever" = "fever",
-                        "Cough" = "cough",
-                        "Sore throat" = "sore_throat",
-                        "Dyspnea" = "dyspnea",
-                        "Respiratory discomfort" = "resp_disc",
-                        "Desaturation" = "desaturation",
-                        "Diarrhea" = "diarrhea",
-                        "Vomit" = "vomit",
-                        "Abdominal pain" = "abd_pain",
-                        "Fatigue" = "fatigue",
-                        "Olfactory loss" = "olfac_loss",
-                        "Loss of taste" = "loss_taste",
-                        "Heart disease" = "cardiac",
-                        "Hematologic" = "hematologic",
-                        "Hepatic" = "hepatic",
-                        "Asthma" = "asthma",
-                        "Diabetes" = "diabetes",
-                        "Neurologic" = "neurologic",
-                        "Lung disease" = "pneumologic",
-                        "Imunossupression" = "imuno",
-                        "Renal" = "renal",
-                        "Obesity" = "obesity",
-                        "ICU admission" = "icu",
-                        "Invasive respiratory support" = "intubation",
-                        "Evolution" = "death"
-                      ),
-                      selected = "death",
-                      width = "220px"
-                    ),
-                    hr(),
-                    sliderInput(
-                      inputId = "idade2",
-                      label = "Age range (months):",
-                      min = min(data5$age_month),
-                      max = max(data5$age_month),
-                      value = c(min(data5$age_month), max(data5$age_month))
-                    )
-                  ),
-                  box(
-                    width = 8,
-                    status = "primary",
-                    div(tabsetPanel(
-                      tabPanel("Cross-tabulation",
-                               highcharter::highchartOutput("plot21"),
-                               verbatimTextOutput("table2"),
-                               h3(strong("Fisher's test")),
-                               verbatimTextOutput("print2"))
-                    )),
-                    h3(strong("Obs")),
-                    p(
-                      "<NA> in the table above indicates the missing or ignored (non-response) cases of the variables in question. In the chart, this information appears in the numbered category (for example, number 2)."
-                    )
-                  )
-                )
-                
+        # Age analysis
+        tabItem(
+          tabName = "agn",
+          tabPanel("pdf", tags$iframe(style = "height:800px; width:100%", src = "age_analysis.pdf"))
         ),
-        ### Analysis with Downsample ----
-        tabItem(tabName = "anal_downsample",
-                fluidRow(
-                  box(
-                    collapsible = TRUE,
-                    width = 4,
-                    title = "Select",
-                    status = "primary",
-                    solidHeader = FALSE,
-                    selectInput(
-                      inputId = "caracteristicas3",
-                      label = "Row variable:",
-                      choices = c(
-                        "Brazil region"  = "region",
-                        "Federative unit of Brazil"  = "SG_UF",
-                        "Ethnicity" = "ethnicity",
-                        "Sex" = "sex",
-                        "Travel history" = "travel_hist",
-                        "Change of municipality for service" = "changed_muni",
-                        "Area of residence" = "zone",
-                        "Flu syndrome evolved into SRAG" = "flu_into_srag",
-                        "Infection acquired in the hospital" = "inf_inter",
-                        "Contact with poultry or swine" = "cont_pty_swine",
-                        "Flu vaccine" = "vacine",
-                        "Antiviral" = "antiviral",
-                        "Fever" = "fever",
-                        "Cough" = "cough",
-                        "Sore throat" = "sore_throat",
-                        "Dyspnea" = "dyspnea",
-                        "Respiratory discomfort" = "resp_disc",
-                        "Desaturation" = "desaturation",
-                        "Diarrhea" = "diarrhea",
-                        "Vomit" = "vomit",
-                        "Abdominal pain" = "abd_pain",
-                        "Fatigue" = "fatigue",
-                        "Olfactory loss" = "olfac_loss",
-                        "Loss of taste" = "loss_taste",
-                        "Heart disease" = "cardiac",
-                        "Hematologic" = "hematologic",
-                        "Hepatic" = "hepatic",
-                        "Asthma" = "asthma",
-                        "Diabetes" = "diabetes",
-                        "Neurologic" = "neurologic",
-                        "Lung disease" = "pneumologic",
-                        "Imunossupression" = "imuno",
-                        "Renal" = "renal",
-                        "Obesity" = "obesity",
-                        "ICU admission" = "icu",
-                        "Invasive respiratory support" = "intubation",
-                        "Evolution" = "death"
-                      ),
-                      selected = "death",
-                      width = "220px"
-                    ),
-                    hr(),
-                    sliderInput(
-                      inputId = "idade3",
-                      label = "Age range (months):",
-                      min = min(data5$age_month),
-                      max = max(data5$age_month),
-                      value = c(min(data5$age_month), max(data5$age_month))
-                    )
-                  ),
-                  box(
-                    width = 8,
-                    status = "primary",
-                    div(tabsetPanel(
-                      tabPanel("Cross-tabulation",
-                               highcharter::highchartOutput("plot31"),
-                               verbatimTextOutput("table3"),
-                               h3(strong("Fisher's test")),
-                               verbatimTextOutput("print3")
-                      )
-                    )),
-                    h3(strong("Obs")),
-                    p(
-                      "<NA> in the table above indicates the missing or ignored (non-response) cases of the variables in question. In the chart, this information appears in the numbered category (for example, number 2)."
-                    )
-                  )
-                )
-                
-        ),
-        
         ### Models ----
         tabItem(tabName = "modelos",
                 fluidRow(
@@ -472,6 +279,9 @@ ui <-
                     div(tabsetPanel(
                       tabPanel("Normal logistic model",
                                verbatimTextOutput("print7"),
+                               verbatimTextOutput("print72"),
+                               h3(strong("Forest plot")),
+                               plotOutput("print73"),
                                h3(strong("Envelope Chart")),
                                plotOutput("plot4")
                                )
@@ -565,135 +375,24 @@ server <- function(input, output, session) {
   output$print7 <- renderPrint({
     summary(glm(data = selectData5(), as.factor(get(input$caracteristicas4)) ~ variants, family = binomial))
   })
+
+  output$print72 <- renderPrint({
+    model_l <- glm(data = selectData5(), as.factor(get(input$caracteristicas4)) ~ variants, family = binomial)
+    exp(cbind("Odds ratio" = coef(model_l), confint.default(model_l, level = 0.95)))
+  })
+  
+  output$print73 <- renderPlot({
+    model_l <- glm(data = selectData5(), as.factor(get(input$caracteristicas4)) ~ variants, family = binomial)
+    set_theme(base = theme_classic(), #To remove the background color and the grids
+              theme.font = 'sans serif',   #To change the font type
+              axis.title.size = 1.4,  #To change axis title size
+              axis.textsize.x = 1.4,  #To change x axis text size
+              axis.textsize.y = 1.4)  #To change y axis text size
+    plot_model(model_l, show.values = TRUE, value.offset = .3, vline.color = "red", title = "Forest")
+  })
   
   output$plot4 <- renderPlot({
     hnp::hnp(glm(data = selectData5(), as.factor(get(input$caracteristicas4)) ~ variants, family = binomial))
-  })
-  
-  ## Dataset with input filtering (smote) ----
-  
-  selectData_smote <- reactive({
-    data5 %>%
-      dplyr::filter(age_month >= input$idade2[1]) %>%
-      dplyr::filter(age_month <= input$idade2[2]) 
-  })
-  
-  ### Cross-table/plot (original) ----
-  
-  selectData3 <- reactive({
-    dados_smote(selectData_smote(),input$caracteristicas2) #%>% 
-      #dplyr::filter(variants %in% input$classivariants2)  
-  })
-  
-  dados_hc_aux2 <- reactive({
-    selectData3() %>%
-      count(var = .[["variants"]]) %>%
-      mutate(ntot = n) %>%
-      select(-n)
-  })
-  
-  dados_hc2 <- reactive({
-    selectData3() %>%
-      count(var = .[["variants"]],
-            var2 = .[[input$caracteristicas2]]) %>%
-      full_join(dados_hc_aux2(), by = "var") %>%
-      mutate(porc = round((n / ntot) * 100, 2))
-  })
-  
-  output$plot21 <- highcharter::renderHighchart({
-    hchart(dados_hc2(), type = "column",
-           hcaes(x = var,
-                 y = porc, group = var2)) %>%
-      hc_xAxis(title = list(text = "variants")) %>%
-      hc_yAxis(title = list(text = "%")) %>%
-      hc_add_theme(hc_theme_elementary())
-  })
-  
-  
-  output$table2 <- renderPrint({
-    st_options(headings = FALSE, display.labels = FALSE)
-    with(
-      selectData3(),
-      summarytools::ctable(
-        get(input$caracteristicas2),
-        variants,
-        prop = "c",
-        headings = st_options("headings"),
-        display.labels = st_options("display.labels"),
-        useNA = "ifany",
-        dnn = c(input$caracteristicas2, "variants"),
-        OR = TRUE,
-        chisq = TRUE
-      )
-    )
-  })
-  
-  output$print2 <- renderPrint({
-    with(selectData3(),
-         fisher.test(variants,get(input$caracteristicas2),simulate.p.value = TRUE))
-  })
-  
-  ## Dataset with input filtering (downsample) ----
-  
-  selectData_dowsample <- reactive({
-    data5 %>%
-      dplyr::filter(age_month >= input$idade3[1]) %>%
-      dplyr::filter(age_month <= input$idade3[2]) 
-  })
-  
-  ### Cross-table/plot (original) ----
-  
-  selectData4 <- reactive({
-    dados_downsample(selectData_dowsample(),input$caracteristicas3) #%>% 
-     # dplyr::filter(variants %in% input$classivariants3) 
-  })
-  
-  dados_hc_aux3 <- reactive({
-    selectData4() %>%
-      count(var = .[["variants"]]) %>%
-      mutate(ntot = n) %>%
-      select(-n)
-  })
-  
-  dados_hc3 <- reactive({
-    selectData4() %>%
-      count(var = .[["variants"]],
-            var2 = .[[input$caracteristicas3]]) %>%
-      full_join(dados_hc_aux3(), by = "var") %>%
-      mutate(porc = round((n / ntot) * 100, 2))
-  })
-  
-  output$plot31 <- highcharter::renderHighchart({
-    hchart(dados_hc3(), type = "column",
-           hcaes(x = var,
-                 y = porc, group = var2)) %>%
-      hc_xAxis(title = list(text = "Variants")) %>%
-      hc_yAxis(title = list(text = "%")) %>%
-      hc_add_theme(hc_theme_elementary())
-  })
-  
-  
-  output$table3 <- renderPrint({
-    st_options(headings = FALSE, display.labels = FALSE)
-    with(
-      selectData4(),
-      summarytools::ctable(
-        get(input$caracteristicas3),
-        variants,
-        prop = "c",
-        headings = st_options("headings"),
-        display.labels = st_options("display.labels"),
-        useNA = "ifany",
-        dnn = c(input$caracteristicas3, "Variants"),
-        OR = TRUE,
-        chisq = TRUE
-      )
-    )
-  })
-  
-  output$print3 <- renderPrint({
-    with(selectData4(),
-         fisher.test(variants,get(input$caracteristicas3),simulate.p.value = TRUE))
   })
   
   # Gather all the form inputs (and add timestamp)
